50e15752ce0801c0f77477e3df60dacb77d29c76
GRodriguezPardo <gonrpardo@hotmail.com>
2021-05-05 00:27:42 -0300

    Agrego el atributo fecha

--- src/main/java/primeraIteracion/personas/Rescatista.java
+++ src/main/java/primeraIteracion/personas/Rescatista.java
@@ -6,10 +6,12 @@ import java.util.Objects;
 import primeraIteracion.mascotas.MascotaPerdida;
 
 /**
- * Es una clase de tipo de persona pero capaz de poseer una o mas mascotas.
+ * Es una clase que representa una situacion de mascota perdida encontrada.
+ * Posee informacion sobre la persona
  */
 public class Rescatista extends Persona {
   private MascotaPerdida mascota;
+  private LocalDate fecha;
 
   /**
    * Constructor de la clase.
@@ -19,16 +21,20 @@ public class Rescatista extends Persona {
    * @param _nombreYApellido es el nombre y apellido de la persona.
    * @param _fechaNacimiento es la fecha de nacimiento de la persona.
    * @param _contacto es la lista de formas de contacto que tiene la persona.
+   * @param _fecha es la fecha en la que reporto la mascota perdida.
    * @param _mascota es la mascota rescatada por el rescatista.
    */
   public Rescatista (String _nombreYApellido,
                  LocalDate _fechaNacimiento,
                  List<Contacto> _contacto,
+                 LocalDate _fecha,
                  MascotaPerdida _mascota) {
     super(_nombreYApellido, _fechaNacimiento, _contacto);
-    if(Objects.isNull(_mascota)){
+    if(Objects.isNull(_mascota)
+       || Objects.isNull(_fecha)){
       //TODO
     }
     this.mascota = _mascota;
+    this.fecha = _fecha;
   }
 }
