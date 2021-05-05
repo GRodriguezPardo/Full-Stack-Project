6aa325b14a1fe859c1883129e4cefafbc9eae01b
GRodriguezPardo <gonrpardo@hotmail.com>
2021-05-05 15:47:28 -0300

    Agrego documentacion de clases y metodos

--- src/main/java/primeraIteracion/mascotas/PosiblesCaracteristicas.java
+++ src/main/java/primeraIteracion/mascotas/PosiblesCaracteristicas.java
@@ -20,16 +20,31 @@ public class PosiblesCaracteristicas {
 
   /**
    * Metodo estatico para obtener al singleton.
+   *
    * @return retorna al singleton.
    */
   public static PosiblesCaracteristicas getInstance() {
     return INSTANCE;
   }
 
+  /**
+   * Agrega una nueva posible Caracteristica al hashMap de caracteristicas
+   * bajo el nombre pasado porparametro.
+   *
+   * @param nombre nombre que referenciara a la nueva Caracteristica.
+   * @param nuevaCaracteristica nueva psoible Caracteristica.
+   */
   public void agregarPosibleCaracteristica(String nombre, Caracteristica nuevaCaracteristica) {
     this.caracteristicas.put(nombre, nuevaCaracteristica);
   }
 
+  /**
+   * Busca la caracteristica referenciada por el nombre pasado por parametro
+   * y devulve un clon de la misma.
+   *
+   * @param nombre es el nombre de la Caracteristica que se busca.
+   * @return retorna un clon de la caracteristica buscada.
+   */
   public Caracteristica definirCaracteristica(String nombre) {
     Caracteristica caracteristica = caracteristicas.get(nombre);
     return caracteristica.clone();
