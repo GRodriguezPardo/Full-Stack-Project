250945f9934af05d81357c3a7126bc9b18e8da39
NicolasPabloW <nicokido2011@gmail.com>
2021-05-06 19:03:18 -0300

    Requermiento del top d econtraseñias semi completo

---
+++ src/test/java/ValidacionesContraseniaTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import primeraIteracion.seguridad.ValidacionesDeSeguridad;
+
+public class ValidacionesContraseniaTest {
+
+  ValidacionesDeSeguridad validaciones = new ValidacionesDeSeguridad();
+
+  @Test
+  private void rebotarContraseniaDebil(){
+validaciones.verificarQueEsContraseniaFuerte(" ");
+
+
+
+  }
+
+}
