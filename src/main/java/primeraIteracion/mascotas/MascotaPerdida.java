adb3e35dee5f723376a8e22468faed36e7ffc45c
GRodriguezPardo <gonrpardo@hotmail.com>
2021-05-06 16:50:52 -0300

    Agrego las fotos a mascotas perdidas

--- src/main/java/primeraIteracion/mascotas/MascotaPerdida.java
+++ src/main/java/primeraIteracion/mascotas/MascotaPerdida.java
@@ -1,25 +1,35 @@
 package primeraIteracion.mascotas;
 
+import java.util.List;
+import javafx.scene.image.Image;
+import primeraIteracion.exceptions.FaltanDatosException;
+
 /**
  * Clase que representa una mascota perdida.
  */
 public class MascotaPerdida {
-  //private List<Foto> fotos;
   private String descripcionEstado;
+  private List<Image> fotos;
   private Integer posicionX;
   private Integer posicionY;
 
   /**
    * Constructor de la clase.
    *
+   * @param _fotos son las fotos de la mascota perdida, minimo se necesita una.
    * @param descripcionEstado es una descripcion del estado en que se encontro a la mascota.
    * @param posicionX es la posicion en X de la localizacion de la mascota encontrada.
    * @param posicionY es la posicion en Y de la localizacion de la mascota encontrada.
    */
-  public MascotaPerdida(String descripcionEstado,
+  public MascotaPerdida( String descripcionEstado,
+                        List<Image> _fotos,
                         Integer posicionX,
                         Integer posicionY) {
     this.descripcionEstado = descripcionEstado;
+    if (_fotos.isEmpty()){
+      throw new FaltanDatosException("Se necesita proveer minimo una foro");
+      }
+    this.fotos = _fotos;
     this.posicionX = posicionX;
     this.posicionY = posicionY;
   }
