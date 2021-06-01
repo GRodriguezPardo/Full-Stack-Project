46c649c74b7010abc3c3a6f6ca288fcde613f5d4
GRodriguezPardo <gonrpardo@hotmail.com>
2021-06-01 16:40:03 -0300

    Quito un misplaced method en Admin

--- src/main/java/primeraIteracion/personas/Admin.java
+++ src/main/java/primeraIteracion/personas/Admin.java
@@ -7,9 +7,4 @@ public class Admin extends Perfil {
   public Admin(String usuario, String clave) {
     super(usuario, clave);
   }
-
-  public void nuevaCaracteristica(String nombre, Caracteristica nuevaCaracteristica) {
-    PosiblesCaracteristicas.getInstance()
-      .agregarPosibleCaracteristica(nombre, nuevaCaracteristica);
-  }
 }
