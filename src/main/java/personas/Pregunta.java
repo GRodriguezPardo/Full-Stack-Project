4054491367c6cd7e80afadadd058c238f22cf1f4
NicolasPabloW <nicokido2011@gmail.com>
2021-06-26 21:38:23 -0300

    Modifico pregunta

--- src/main/java/personas/Pregunta.java
+++ src/main/java/personas/Pregunta.java
@@ -5,6 +5,7 @@ import repositorios.RepositorioDePreguntas;
 public class Pregunta {
   String cuerpo;
   boolean respuesta;
+  boolean visible = true;
   boolean contestada = false; /*Porque el booleano de respuesta siempre estara en false o true (alguno de los 2 es
   valor default al no setear el atributo y no se sabria si se contesto la pregunta o no)*/
 
@@ -20,4 +21,15 @@ public class Pregunta {
     return this.cuerpo;
   }
 
+  public void responder(Boolean respuesta) {
+    this.respuesta = respuesta;
+  }
+
+  public void ocultar() {
+    this.visible = false;
+  }
+
+  public void mostrar() {
+    this.visible = true;
+  }
 }
