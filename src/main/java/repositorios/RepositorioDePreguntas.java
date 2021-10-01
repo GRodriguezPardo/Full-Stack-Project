800d2b2307c79104d922008504d39eaa352cee81
GRodriguezPardo <gonrpardo@hotmail.com>
2021-10-01 16:57:31 -0300

    Actualizo repo preguntas

--- src/main/java/repositorios/RepositorioDePreguntas.java
+++ src/main/java/repositorios/RepositorioDePreguntas.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class RepositorioDePreguntas implements WithGlobalEntityManager {
   private final static RepositorioDePreguntas INSTANCE = new RepositorioDePreguntas();
-  private final List<Pregunta> preguntas = new ArrayList<>();
+  //private final List<Pregunta> preguntas = new ArrayList<>();
 
   private RepositorioDePreguntas() {
   }
@@ -20,8 +20,7 @@ public class RepositorioDePreguntas implements WithGlobalEntityManager {
   }
 
   public void agregarPregunta(Pregunta pregunta) {
-    ;
-    if (Objects.isNull(entityManager().find(Pregunta.class, pregunta))) {
+    if (Objects.isNull(entityManager().find(Pregunta.class, pregunta.getId()))) {
       this.persisirPregunta(pregunta);
     }
   }
@@ -32,21 +31,24 @@ public class RepositorioDePreguntas implements WithGlobalEntityManager {
 
   public void eliminarPregunta(Pregunta pregunta) {
     //this.preguntas.remove(pregunta);
-    Pregunta preguntaABorrar = entityManager().find(Pregunta.class, pregunta);
+    Pregunta preguntaABorrar = entityManager().find(Pregunta.class, pregunta.getId());
     entityManager().remove(preguntaABorrar);
   }
 
   public void borrarPreguntas() {
-    this.preguntas.clear();
+    //this.preguntas.clear();
+    this.getPreguntas().forEach(unaPregunta -> this.eliminarPregunta(unaPregunta));
   }
 
   public List<Pregunta> getPreguntas() {
-    return this.preguntas;
+    //return this.preguntas;
+    return entityManager()
+        .createQuery("from Pregunta").getResultList();
   }
   /*El metodo de abajo es para que el que gestiona las preguntas globales vea si ya hizo preguntas
   que esta pensando o tiene mejores o si no. */
 
   public List<Pregunta> verPreguntasQueIncluyanCadena(String cadena) {
-    return this.preguntas.stream().filter(p -> p.getCuerpoDuenio().contains(cadena)).collect(Collectors.toList());
+    return this.getPreguntas().stream().filter(p -> p.getCuerpoDuenio().contains(cadena)).collect(Collectors.toList());
   }
 }
