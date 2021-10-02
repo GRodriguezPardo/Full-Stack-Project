5d79aeada78f8a24d158b39600cce7c9b2c43585
NicolasPabloW <nicokido2011@gmail.com>
2021-10-02 03:19:54 -0300

    Intercambio nombres twilio y smser

--- src/test/java/Fixture.java
+++ src/test/java/Fixture.java
@@ -1,6 +1,6 @@
 import apis.Mailer;
 import apis.MedioNotificacion;
-import apis.TwilioJava;
+import apis.Smser;
 import mascotas.*;
 import personas.*;
 import repositorios.RepositorioDePreguntas;
@@ -15,7 +15,7 @@ import static org.mockito.Mockito.mock;
 
 public class Fixture {
   MedioNotificacion emailSender = mock(Mailer.class);
-  MedioNotificacion smsSender = mock(TwilioJava.class);
+  MedioNotificacion smsSender = mock(Smser.class);
 
   MedioNotificacion getEmailSenderMock() {
     return this.emailSender;
--- src/test/java/NotificacionesTest.java
+++ src/test/java/NotificacionesTest.java
@@ -1,16 +1,12 @@
 import apis.JavaXMail;
 import apis.Smser;
-import apis.TwilioJava;
-import apis.dto.HogarDTO;
 import apis.Mailer;
+import apis.TwilioJava;
 import mascotas.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import personas.Contacto;
-import personas.Posicion;
-import services.HogaresService;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -33,9 +29,9 @@ public class NotificacionesTest {
   @Test
   @Disabled
   public void enviarSmsNoTiraErrorTESTMANUAL() {
-    TwilioJava twilio = new TwilioJava(new Smser(null, null, null));//LEER comentarios en smsSender para probar posta con tu telefono
+    Smser sender = new Smser(new TwilioJava(null, null, null));//LEER comentarios en smsSender para probar posta con tu telefono
     Contacto contacto = new Contacto("Anonimo", "+541165919737", "messi@messi.com");
-    assertDoesNotThrow(() -> twilio.notificarMascotaPerdida(contacto));
+    assertDoesNotThrow(() -> sender.notificarMascotaPerdida(contacto));
   }  /*Ahi pones un numero destinatario verificado en la pagina (ese lo esta pero no vas a ver el mensaje , es para mostrar el formato
   valido del numero) y te fijas que te llege el mensaje*/
 
