012cc418cc9f377bd100d867af668c6f914970cf
NicolasPabloW <nicokido2011@gmail.com>
2021-11-19 02:09:36 -0300

    Ya anda agregar carateristicas

--- src/main/java/controllers/CaracteristicaController.java
+++ src/main/java/controllers/CaracteristicaController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import mascotas.PosibleCaracteristica;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 import personas.Rescatista;
 import repositorios.RepositorioDeCaracteristicas;
 import repositorios.RepositorioDeMascotas;
@@ -13,7 +14,7 @@ import spark.Response;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class CaracteristicaController implements WithGlobalEntityManager {
+public class CaracteristicaController implements WithGlobalEntityManager , TransactionalOps {
 
   public Map<String, Object> obtenerSesion(Request request, Response response) {
     Map<String, Object> model = new HashMap<>();
@@ -33,8 +34,10 @@ public class CaracteristicaController implements WithGlobalEntityManager {
     RepositorioDeCaracteristicas.getInstance().agregarPosibleCaracteristica(request.queryParams("nombre"));
 
     //lo de abajo es porque no funciona haciendolo con el repositorio
-
-      entityManager().persist(new PosibleCaracteristica(request.queryParams("nombre")));
+    withTransaction(() -> {
+               // RepositorioDeCaracteristicas.getInstance().agregarPosibleCaracteristica(request.queryParams("nombre"));
+              entityManager().persist(new PosibleCaracteristica(request.queryParams("nombre")));
+            });
     response.redirect("/caracteristicas"); return  null;
     //return new ModelAndView(obtenerSesion(request, response), "caracteristicas/caracteristicas.html.hbs");
   }
